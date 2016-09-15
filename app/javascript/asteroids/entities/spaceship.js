9849a20d4f67151d3ed98667762cc11085d35d8f
kzsh <kzsh@kzsh.dev>
2016-09-15 08:35:12 -0400

    Put some test entities into the Game

---
+++ app/javascript/asteroids/entities/spaceship.js
@@ -0,0 +1,35 @@
+import Entity from 'engine/entities/entity';
+
+export default class Spaceship extends Entity {
+
+  constructor(x, y, dx, dy, r, mass, color) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+
+    this.r = r;
+    this.mass = mass;
+    this.color = color;
+  }
+
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  draw(context) {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.fillStyle = '#f00';
+    context.beginPath();
+    context.moveTo(this.x, this.y);
+    context.lineTo(this.x + 100, this.y + 50);
+    context.lineTo(this.x + 50, this.y + 100);
+    context.lineTo(this.x + 0, this.y + 90);
+    context.closePath();
+    context.fill();
+  }
+
+}
--- app/javascript/asteroids/game/index.js
+++ app/javascript/asteroids/game/index.js
@@ -1,6 +1,35 @@
+import Ball from 'asteroids/entities/ball'
+import Spaceship from 'asteroids/entities/spaceship'
+import { getWindowDimensions } from 'asteroids/utils/dom';
 export default class Game {
   constructor({engine}) {
     this.engine = engine;
+
+
+    for(let x = 0; x < 10; x++) {
+      this.engine.addEntity(
+        new Ball(
+          getRandomPosition().x,
+          getRandomPosition().y,
+          getRandomVelocity(0.1).dx,
+          getRandomVelocity(0.1).dy,
+          Math.floor(Math.random() * 10),
+          1,
+          "#000")
+      );
+    }
+    this.engine.addEntity(
+      new Spaceship(
+        getRandomPosition().x,
+        getRandomPosition().y,
+        getRandomVelocity(0.1).dx,
+        getRandomVelocity(0.1).dy,
+        Math.floor(Math.random() * 10),
+        1,
+        "#000")
+    );
+    let started = true;
+    this.start();
 
   }
 
@@ -12,4 +41,22 @@ export default class Game {
     this.engine.stop();
   }
 
+}
+
+function getRandomPosition() {
+  const dim = getWindowDimensions();
+  return {
+    x: Math.floor(Math.random() * dim.w),
+    y: Math.floor(Math.random() * dim.h)
+  }
+}
+
+function getRandomVelocity(maxX, maxY) {
+  if (!maxY) {
+    maxY = maxX;
+  }
+  return {
+    dx: (Math.random() * maxX * 5) - maxX,
+    dy: (Math.random() * maxY * 5) - maxY
+  }
 }
