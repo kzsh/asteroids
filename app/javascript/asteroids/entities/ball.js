c06b5a21a809e19c69aa5cbb8c321f2078f29d8c
Andrew Hunt <andrew@hunt.li>
2018-04-18 21:33:54 -0400

    Hack gravitation into Ball

--- app/javascript/asteroids/entities/ball.js
+++ app/javascript/asteroids/entities/ball.js
@@ -1,6 +1,7 @@
 import Entity from 'engine/entities/entity';
 import Circle from 'engine/system/shape/circle';
 
+const gravitationalFactor = 1;
 export default class Ball extends Entity {
 
   constructor(x, y, dx, dy, r, mass, color) {
@@ -9,15 +10,36 @@ export default class Ball extends Entity {
     this.y = y;
     this.dx = dx;
     this.dy = dy;
+    this.ddy = 0;
+    this.ddx = 0;
 
     this.r = r;
     this.mass = mass;
     this.color = color;
   }
 
-  update() {
-    this.x += this.dx;
-    this.y += this.dy;
+  update(delta, entities) {
+    let ddx = 0;
+    let ddy = 0;
+    let velocityFactor = delta * 50;
+
+    for(let e of entities) {
+      let r = Math.sqrt(Math.pow(this.x - e.x, 2) + Math.pow(this.y - e.y, 2));
+      if (r < e.r || r < this.r) {
+        ddx = ddy = 0;
+      } else {
+        ddx += ((e.x - this.x) * e.mass * this.mass * gravitationalFactor / Math.pow(r, 3)) || 0;
+        ddy += ((e.y - this.y) * e.mass * this.mass * gravitationalFactor / Math.pow(r, 3)) || 0;
+      }
+    }
+
+    this.x += this.dx*velocityFactor;
+    this.y += this.dy*velocityFactor;
+
+    this.dx += ddx;
+    this.dy += ddy;
+    this.ddx = ddx * delta;
+    this.ddy = ddy * delta;
   }
 
   render(context) {
